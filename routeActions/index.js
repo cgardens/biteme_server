717c6879d2cda11329f214cae05a2b6eb5228120
Charles & Quynh Chen <pair+cgardens.quynhchen@devbootcamp.com>
2015-02-13 13:00:16 -0800

    working on http recall to big oven from backend

--- routeActions/index.js
+++ routeActions/index.js
@@ -1,4 +1,7 @@
 var User = require('../schemas/user');
+var http = require('http');
+var dotenv = require('dotenv');
+dotenv.load()
 
 module.exports = function () {
 
@@ -13,6 +16,54 @@ module.exports = function () {
         res.json(users);
       }
     });
+  };
+
+  functions.searchRecipes = function (req, res) {
+
+    console.log('in search recipes');
+    // var querystring='hi'
+    // var postData = querystring.stringify({
+    //   'msg' : 'Hello World!'
+    // });
+    var postData = 'hi'
+
+
+    var apiKey = process.env.BIG_OVEN_API_KEY
+    console.log(apiKey);
+    // var url = "http://api.bigoven.com/recipe/" + 196149 + "?api_key=" + apiKey
+    // console.log(url);
+    var recipeID = 196149;
+
+    var path = "/recipe/" + recipeID + "?api_key="+ apiKey
+
+    var options = {
+      hostname: "http://api.bigoven.com/",
+      port: 80,
+      path: path,
+      method: 'GET',
+      headers: {
+        dataType: 'json',
+        cache: false,
+      }
+    }
+
+    var req = http.request(options, function(res) {
+      console.log('STATUS' + res.statusCode);
+      console.log('HEADERS' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) {
+        console.log('BODY' + chunk);
+      });
+    });
+
+    console.log(req)
+
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    })
+
+    req.write(postData);
+    req.end();
   };
 
   return functions;
