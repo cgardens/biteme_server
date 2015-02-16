7ef346c101e199d9b015b47a93a65a8ce11d8b32
QuynhChen <quynh.chen@gmail.com>
2015-02-16 15:57:42 -0800

    cors work now, routes moved below cors config

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var path      = require('path');
 var morgan    = require("morgan");
 var mongoose  = require("mongoose");
 var app       = express();
-var routes    = require('./routes')(app);
 
 // all environments
 var port = process.env.PORT || 3000;
@@ -18,13 +17,14 @@ console.log('Node express server running on port ' + port);
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Origin, content-type, Authorization');
   // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
 
   next();
 });
 
+var routes    = require('./routes')(app);
 
 
 
