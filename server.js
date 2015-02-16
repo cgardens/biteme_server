824ae1cde64fcc406d606ecc66a223b011ae7e56
QuynhChen <quynh.chen@gmail.com>
2015-02-15 21:59:56 -0800

    add body parser module to access data sent with post requests

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ var dotenv    = require('dotenv');
 dotenv.load();
 
 //initialize app
-var express   = require('express');
-var path      = require('path');
-var morgan    = require("morgan");
-var mongoose  = require("mongoose");
-var app       = express();
-var routes    = require('./routes')(app);
-
+var express     = require('express');
+var path        = require('path');
+var morgan      = require("morgan");
+var bodyParser  = require('body-parser');
+var mongoose    = require("mongoose");
+var app         = express();
 
 // all environments
 var port = process.env.PORT || 9000;
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
 console.log('Node express server running on port ' + port);
+
+// Routes need to be setup after configuring Express environment
+var routes = require('./routes')(app);
 
 //CORS Configuration
 app.use(function(req, res, next) {
